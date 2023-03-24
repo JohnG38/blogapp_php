377bf36aa50a9f5292c99decba33bab03a3d157f
John <john@john-ubuntu.home>
2023-03-24 11:06:59 +0100

    feat: affichage de la liste des articles

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
+<?php
+    $filename = __DIR__.'/data/articles.json';
+    $articles = [];
 
+    if(file_exists($filename)) {
+        $articles = json_decode(file_get_contents($filename), true) ?? [];
+    }
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require_once "includes/head.php" ?>
+    <?php require_once 'includes/head.php' ?>
     <link rel="stylesheet" href="public/css/index.css">
-    <title>BlogApp</title>
+    <title>BLOG App</title>
 </head>
 <body>
     <div class="container">
         <?php require_once "includes/header.php" ?>
         <div class="content">
-            
+            <div class="articles-container">
+                <?php foreach($articles as $article) :  ?>
+                    <div class="article block">
+                        <div class="overflow">
+                            <div class="img-container" style="background-image: url(<?= $article['image'] ?>)"  ></div>
+                            <h2><?= $article['title'] ?></h2>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
-        <?php require_once "includes/footer.php" ?>        
+        <?php require_once "includes/footer.php" ?>
     </div>
 </body>
 </html>
