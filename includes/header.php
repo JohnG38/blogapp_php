b72b77e9e738a693370e095fbadaa1cc709d5216
John <john@john-ubuntu.home>
2023-04-04 11:09:18 +0200

    feat: mise en place d'un systeme d'authentification, inscription de user

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,18 @@
     <a href="/" class="logo">BLOG APP</a>
     <ul class="header-menu">
         <li class="<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>">
-            <a href="/form-article.php">Ajouter un article</a>
+            <a href="/form-article.php">Add a article</a>
         </li>
+        <li class="<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? 'active' : '' ?>">
+            <a href="/auth-login.php">Login</a>
+        </li>
+        <li class="<?= $_SERVER['REQUEST_URI'] === '/auth-logout.php' ? 'active' : '' ?>">
+            <a href="/auth-logout.php">Logout</a>
+        </li>
+        <li class="<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? 'active' : '' ?>">
+            <a href="/auth-register.php">Register</a>
+        </li>
+
     </ul>
 </header>
     
