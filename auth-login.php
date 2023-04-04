67bff9c769198cea4c22adecd266cc4bd551851c
John <john@john-ubuntu.home>
2023-04-04 11:43:59 +0200

    feat: mise en place du login et creation de la session

--- auth-login.php
+++ auth-login.php
@@ -1,5 +1,67 @@
 <?php
+
+    /**
+     * @var AuthDAO
+     */
+
+    $authDAO = require_once './database/models/AuthDAO.php';
+
+    const ERROR_REQUIRED = "Veuillez renseigner ce champ";
+    const ERROR_EMAIL_INVALID = "L'email n'est pas valide";
+    const ERROR_EMAIL_UNKNOWN = "Cet email n'existe pas.";
+    const ERROR_PASSWORD_MISMATCH = "L'email et/ou le mot de passe sont erronés.";
+
+    $errors = [
+        'email' => '',
+        'password' => '',
+    ];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $input = filter_input_array(INPUT_POST, [
+            'email' => FILTER_SANITIZE_EMAIL
+        ]);
+
+        $email = $input["email"] ?? '';
+        $password = $_POST["password"] ?? '';
+
+
+
+        if(!$email) {
+            $errors['email'] = ERROR_REQUIRED;
+        } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = ERROR_EMAIL_INVALID;
+        }
+
+        if(!$password) {
+            $errors['password'] = ERROR_REQUIRED;
+        }
+
+
+        if(empty(array_filter($errors, fn ($e) => $e !== ''))) {
+
+            //  logger le user
+            $user = $authDAO->getUser($email);
+
+            if(!$user) {
+                $errors['email'] = ERROR_EMAIL_UNKNOWN;
+            } else {
+                if(!password_verify($password, $user['password'])) {
+                    $errors['password'] = ERROR_PASSWORD_MISMATCH;
+                } else {
+
+                    // on crée une nouvelle session
+                    $sessionId = $authDAO->createSession($user['id']);
+
+                    // on crée notre cookie
+                    setcookie('session', $sessionId, time() + 60 * 60 * 24 * 14, "", "", false, true);
+
+                    header('Location: /');
+                }
+            }
+        }
+    }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,9 +74,35 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
+            <div class="block p-20 form-container">
+                    <h1>Login</h1>
+                    <form action="/auth-login.php" method="POST">
+                        <div class="form-control">
 
-        </div>
+                        <div class="form-control">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email">
+                            <?php if($errors['email']) : ?>
+                                <p class="text-error"><?= $errors['email'] ?></p>
+                            <?php endif ; ?>
+                        </div>
+
+                        <div class="form-control">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" id="password">
+                            <?php if($errors['password']) : ?>
+                                <p class="text-error"><?= $errors['password'] ?></p>
+                            <?php endif ; ?>
+                        </div>
+
+                        <div class="form-action">
+                            <a href="/" class="btn btn-secondary">Cancel</a>
+                            <button class="btn btn-primary" type="submit">Connect</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
 </body>
-</html>
+</html>```
